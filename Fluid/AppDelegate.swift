ccbc841c1e783b3ab1cd646a4993c2e147a7b6cb
Bernard Huff <bernard.huff008@gmail.com>
2018-07-17 03:07:46 -0500

    init commit

--- Fluid/AppDelegate.swift
+++ Fluid/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        This is the configuration for using parse with heroku
+        
+        let config = ParseClientConfiguration { (theConfig) in
+            
+//            Accessing Heroku app and keys below
+            theConfig.applicationId = "myFluidApp"
+            theConfig.clientKey = "tRowa12#$"
+            theConfig.server = "http://fluidgram.herokuapp.com/parse"
+        }
+        
+        Parse.initialize(with: config)
+        
+//        parse-dashboard --appId myFluidApp --masterKey tRowa12#$ --serverURL "http://fluidgram.herokuapp.com/parse"
+        
         return true
     }
 
--- Fluid/ViewController.swift
+++ Fluid/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let object = PFObject(className: "testObject")
+        object["name"] = "Bill"
+        object["last name"] = "Foster"
+        object.saveInBackground { (success, error) in
+           print("Save successfully.")
+            
+        } 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
