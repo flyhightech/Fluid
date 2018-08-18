ff27a0971f9e71a931f68a2461d8e3e76c0fb0e7
Bernard Huff <bernard.huff008@gmail.com>
2018-08-17 21:03:47 -0500

    Sweep

--- Fluid/ViewController.swift
+++ Fluid/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Bernard Huff on 7/16/18.
 //  Copyright © 2018 Bernard Huff. All rights reserved.
 //
-//  Test 2
+//  Treat it like a investment
 
 import UIKit
 import Parse
@@ -13,14 +13,29 @@ import Parse
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var picture: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let data = UIImageJPEGRepresentation(picture.image!, 0.5)
+        
+        let file = PFFile(name: "picture.jpg", data: data!)
+        
+        
         let table = PFObject(className: "messages")
         table["sender"] = "Bernard"
         table["receiver"] = "David"
+        table["picture"] = file
+        table["message"] = "Hello King"
+        table.saveInBackground { (success:Bool, error:NSError?) in
+            
+            if success {
+                print("Saved in server...you can continue.")
+            } else {
+                print("You messed up because of this, \(error) . Try a little harder Huff! You got it. 👍🏾")
+            }
+        } as! PFBooleanResultBlock
         
         
     }
